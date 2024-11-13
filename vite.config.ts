892260b57d9d769b9709353563ec8c80a0118fc4
DiegoSt23 <diego.stonerough@gmail.com>
2024-11-13 10:29:45 -0600

    configure vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'delta-ui-2.0',
+      formats: ['es', 'cjs', 'umd'],
+      fileName: 'delta-ui-2.0',
+    },
+    rollupOptions: {
+      external: ['react', 'react dom'],
+    },
+  },
+  plugins: [react(), dts()],
+  resolve: {
+    alias: {
+      '@styles': '/src/styles',
+    },
+  },
+});
